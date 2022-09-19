ba3b670ee1c0b18c3c50729a75ece87933b75ccb
FloScythe <61111800+FloScythe@users.noreply.github.com>
2022-09-19 19:29:15 +0200

    Upgrade boucle

--- Dofus/main.py
+++ Dofus/main.py
@@ -11,23 +11,43 @@ import time
 import keyboard
 
 #Map 1
-liste_x_1 = [637,688,1078,1146,1319,1421,1473]
-liste_y_1 = [689,661,487,455,437,456,546]
+liste_x_1 = [895,1100,1161,1563,1616,]
+liste_y_1 = [726,430,457,637,665]
 ressource_1 = len(liste_x_1)
 
 #Map 2
-liste_x_2 = [637,688,1078,1146,1319,1421,1473]
-liste_y_2 = [689,661,487,455,437,456,546]
+liste_x_2 = [1159,1002,942,735,487,42]
+liste_y_2 = [538,480,513,621,739,764]
 ressource_2 = len(liste_x_2)
 
 #Map 3
-liste_x_3 = [637,688,1078,1146,1319,1421,1473]
-liste_y_3 = [689,661,487,455,437,456,546]
+liste_x_3 = [1356,1305,1265,1138,1083,501,434]
+liste_y_3 = [589,568,554,526,562,645,677]
 ressource_3 = len(liste_x_3)
 
 while keyboard.is_pressed('q') == False:
     time.sleep(random.uniform(0.1, 0.5))
-    for i in range(ressource_1) :
-        pyautogui.moveTo(liste_x_1[i], liste_y_1[i], duration=random.uniform(0.1, 0.3))
-        pyautogui.click()
-        time.sleep(random.uniform(0.1, 0.3))
+    #Map 1
+    for repetitions_1 in range(2):
+        for i in range(ressource_1) :
+            pyautogui.moveTo(liste_x_1[i], liste_y_1[i], duration=random.uniform(0.1, 0.3))
+            pyautogui.click()
+            time.sleep(random.uniform(0.1, 0.3))
+    if keyboard.is_pressed('q'):
+        print("Interruption de la boucle")
+        break
+    #Map 2
+    for repetitions_2 in range(2):
+        for i in range(ressource_1) :
+            pyautogui.moveTo(liste_x_2[i], liste_y_2[i], duration=random.uniform(0.1, 0.3))
+            pyautogui.click()
+            time.sleep(random.uniform(0.1, 0.3))
+    if keyboard.is_pressed('q'):
+        print("Interruption de la boucle")
+        break
+    #Map 3
+    for repetitions_3 in range(2):
+        for i in range(ressource_1) :
+            pyautogui.moveTo(liste_x_3[i], liste_y_3[i], duration=random.uniform(0.1, 0.3))
+            pyautogui.click()
+            time.sleep(random.uniform(0.1, 0.3))
