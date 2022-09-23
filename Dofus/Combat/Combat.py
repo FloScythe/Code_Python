8b8e5ad30c6b806651554ec21649b999b9d35c64
FloScythe <61111800+FloScythe@users.noreply.github.com>
2022-09-23 23:37:21 +0200

    Update minage

--- Dofus/Combat/Combat.py
+++ Dofus/Combat/Combat.py
@@ -3,48 +3,71 @@ import pyautogui
 import time
 import random
 
-def combat():
-    if pyautogui.locateOnScreen("Combat/Pret.png", grayscale=True, confidence=0.8) or pyautogui.locateOnScreen(
-            "Combat/Findetour.png", grayscale=True, confidence=0.8):
 
-        p = pyautogui.locateOnScreen("Combat/Pret.png", confidence=0.8)
-        pyautogui.moveTo(p, duration=random.uniform(0.1, 0.3))
+def souris(nom):
+    z = pyautogui.locateOnScreen(nom, confidence=0.8)
+    pyautogui.moveTo(z, duration=random.uniform(0.1, 0.3))
+    pyautogui.click()
+    time.sleep(0.5)
+
+
+def level_up():
+    # Detection en cas de level up
+    if pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8):
+        up = pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8)
+        pyautogui.moveTo(up, duration=random.uniform(0.1, 0.3))
         pyautogui.click()
 
+
+def combat(x3, y3, x4, y4):
+    if pyautogui.locateOnScreen("../Combat/Pret.png", grayscale=True, confidence=0.8) or pyautogui.locateOnScreen(
+            "../Combat/Findetour.png", grayscale=True, confidence=0.8):
+
+        souris("../Combat/Pret.png")
+
         while True:
-            z = pyautogui.locateOnScreen("Combat/invoc1.png", confidence=0.8)
-            pyautogui.moveTo(z, duration=random.uniform(0.1, 0.3))
-            pyautogui.click()
-
-            time.sleep(random.uniform(0.1, 0.2))
-
-            z1 = pyautogui.locateOnScreen("Combat/Pos1.png", grayscale=False, confidence=0.6)
-            pyautogui.moveTo(z1)
-            z2 = pyautogui.locateOnScreen("Combat/Pos2.png", grayscale=False, confidence=0.6)
-            pyautogui.moveTo(z2)
-            z3 = pyautogui.locateOnScreen("Combat/Pos3.png", grayscale=False, confidence=0.6)
-            pyautogui.moveTo(z3)
-            z4 = pyautogui.locateOnScreen("Combat/Pos4.png", grayscale=False, confidence=0.6)
-            pyautogui.moveTo(z4)
-
-            time.sleep(random.uniform(0.1, 0.2))
-
-            z5 = pyautogui.locateOnScreen("Combat/invoc2.png", confidence=0.8)
-            pyautogui.moveTo(z5, duration=random.uniform(0.1, 0.3))
-            pyautogui.click()
-            pyautogui.click()
-            time.sleep(0.5)
-
-            if pyautogui.pixelMatchesColor(1794,429,(36,48,18)):
-                z6 = pyautogui.locateOnScreen("Combat/Findetour.png", confidence=0.8)
-                pyautogui.moveTo(z6, duration=random.uniform(0.1, 0.3))
+            # if pyautogui.pixelMatchesColor(x1, y1, (73, 97, 37)):
+            #     souris("../Combat/invoc_sanglier.png")
+            #     souris("../Combat/invoc2.png")
+            #     pyautogui.click()
+            # elif pyautogui.pixelMatchesColor(x2, y2, (46, 93, 107)):
+            #     souris("../Combat/invoc_bouf.png")
+            #     souris("../Combat/invoc2.png")
+            #     pyautogui.click()
+            if pyautogui.pixelMatchesColor(x3, y3, (73, 97, 37)):
+                souris("../Combat/invoc_tofu.png")
+                souris("../Combat/invoc2.png")
                 pyautogui.click()
+            elif pyautogui.pixelMatchesColor(x4, y4, (94, 154, 171)):
+                souris("../Combat/Sort_1.png")
+                pyautogui.moveTo(pyautogui.locateOnScreen("../Combat/Perso.png"), confidence=0.8)
+                pyautogui.moveRel(200, None)
+                pyautogui.click()
+            else:
+                z = pyautogui.locateOnScreen("../Combat/Deplacement.png", confidence=0.8)
+                pyautogui.moveTo(z, duration=random.uniform(0.1, 0.3))
+                pyautogui.doubleClick()
+                time.sleep(0.5)
+                souris("../Combat/Findetour.png")
                 time.sleep(1)
 
-            if pyautogui.locateOnScreen("Combat/Quit.png", confidence=0.8):
-                z7 = pyautogui.locateOnScreen("Combat/Quit.png", confidence=0.8)
-                pyautogui.moveTo(z7, duration=random.uniform(0.1, 0.3))
-                pyautogui.click()
+            if pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8):
+                if pyautogui.locateOnScreen("../Combat/dead.png", confidence=0.8):
+                    pyautogui.moveTo(2753, 769)
+                    pyautogui.click()
+                    keyboard.send("ctrl+F2")
+                    time.sleep(1)
+                souris("../Combat/Quit.png")
+                time.sleep(1)
+                if pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8):
+                    level_up()
+                # souris("../Ressource/Inventaire.png")
+                # souris("../Ressource/sac.png")
+                # pyautogui.click()
                 break
             elif keyboard.is_pressed("q"):
+                time.sleep(1)
                 break
+
+
+combat(2109, 418, 2109, 318)
--- Dofus/Métier/Blé.py
+++ Dofus/Métier/Blé.py
@@ -10,4 +10,161 @@ Relancer la boucle
     Vendre si plein
     Bouger de carte
 """
+import time
 
+import keyboard
+import pyautogui
+import random
+
+
+def souris(nom):
+    z = pyautogui.locateOnScreen(nom, confidence=0.8)
+    pyautogui.moveTo(z, duration=random.uniform(0.1, 0.3))
+    pyautogui.click()
+    time.sleep(0.5)
+
+
+def level_up():
+    # Detection en cas de level up
+    time.sleep(1)
+    if pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8):
+        up = pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8)
+        pyautogui.moveTo(up, duration=random.uniform(0.1, 0.3))
+        pyautogui.click()
+        print("Level up")
+
+
+def combat(x3, y3):
+    if pyautogui.locateOnScreen("../Combat/Pret.png", grayscale=True, confidence=0.8) or pyautogui.locateOnScreen(
+            "../Combat/Findetour.png", grayscale=True, confidence=0.8):
+
+        souris("../Combat/Pret.png")
+        print("Debut du combat")
+
+        while True:
+            if pyautogui.pixelMatchesColor(x3, y3, (73, 97, 37)):
+                souris("../Combat/invoc_tofu.png")
+                souris("../Combat/invoc2.png")
+                pyautogui.click()
+            else:
+                z = pyautogui.locateOnScreen("../Combat/Deplacement.png", confidence=0.8)
+                pyautogui.moveTo(z, duration=random.uniform(0.1, 0.3))
+                pyautogui.doubleClick()
+                time.sleep(0.5)
+                souris("../Combat/Findetour.png")
+                time.sleep(1)
+
+            if pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8):
+                if pyautogui.locateOnScreen("../Combat/dead.png", confidence=0.8):
+                    print("T'es mort")
+                    pyautogui.moveTo(2753, 769)
+                    pyautogui.click()
+                    keyboard.send("ctrl+F2")
+                    time.sleep(1)
+                souris("../Combat/Quit.png")
+                time.sleep(1)
+                if pyautogui.locateOnScreen("../Combat/Quit.png", confidence=0.8):
+                    print("")
+                    level_up()
+                souris("../Ressource/Inventaire.png")
+                souris("../Ressource/sac.png")
+                pyautogui.click()
+                break
+            elif keyboard.is_pressed("q"):
+                time.sleep(1)
+                break
+
+
+def sell(x):
+    souris("../Ressource/Inventaire.png")
+    if pyautogui.locateOnScreen("../Ressource/Alerte_full.png", confidence=0.8):
+        souris("../Ressource/Ressource_Inventaire.png")
+        souris(x)
+        souris("../Ressource/hdv.png")
+        souris("../Ressource/QTE100.png")
+        if pyautogui.locateOnScreen("../Ressource/Entrer.png", confidence=0.8):
+            souris("../Ressource/Entrer.png")
+            souris("../Ressource/Mettre_en_vente.png")
+            souris("../Ressource/Vente.png")
+            souris("../Combat/Quit.png")
+        else:
+            souris("../Combat/Quit.png")
+    else:
+        souris("../Combat/Quit.png")
+
+
+i = 0
+BOUCLE = 15
+
+while True:
+    for nombre in range(BOUCLE):
+        ble = pyautogui.locateOnScreen("Ressource_Flo/Ble.PNG", confidence=0.6)
+        orge = pyautogui.locateOnScreen("Ressource_Flo/Orge.PNG", confidence=0.6)
+        avoine = pyautogui.locateOnScreen("Ressource_Flo/Avoine.PNG", confidence=0.6)
+        coupe1 = pyautogui.locateOnScreen("Ressource_Flo/Ble_coupe.PNG", confidence=0.6)
+        # coupe2 = pyautogui.locateOnScreen("Ressource_Flo/Orge_coupe.PNG", confidence=0.6)
+        coupe3 = pyautogui.locateOnScreen("Ressource_Flo/Avoine_coupe.PNG", confidence=0.6)
+
+        pyautogui.moveTo(ble or orge or avoine)
+        pyautogui.mouseDown()
+        time.sleep(1)
+        vide = pyautogui.locateOnScreen("Ressource_Flo/vide.PNG", confidence=0.8)
+        if ble and not vide:
+            i += 1
+            print(f"Recolte Blé : {i}")
+            pyautogui.click()
+            time.sleep(random.uniform(10, 11))
+            pyautogui.mouseUp()
+            level_up()
+            combat(2108,416)
+        elif orge and not vide:
+            i += 1
+            print(f"Recolte Orge : {i}")
+            pyautogui.click()
+            time.sleep(random.uniform(10, 11))
+            pyautogui.mouseUp()
+            level_up()
+            combat(2108,416)
+        elif avoine and not vide:
+            i += 1
+            print(f"Recolte Avoine : {i}")
+            pyautogui.click()
+            time.sleep(random.uniform(10, 11))
+            pyautogui.mouseUp()
+            level_up()
+            combat(2108,416)
+        elif coupe1 or coupe3 or vide:
+            print("Rien trouvé")
+            pyautogui.mouseUp()
+            avoine = pyautogui.locateOnScreen("Ressource_Flo/Avoine.PNG", confidence=0.6)
+            pyautogui.moveTo(avoine)
+            map1 = pyautogui.locateOnScreen("Ressource_Flo/Map1.PNG", confidence=0.8)
+            map2 = pyautogui.locateOnScreen("Ressource_Flo/Map2.PNG", confidence=0.8)
+            map3 = pyautogui.locateOnScreen("Ressource_Flo/Map3.PNG", confidence=0.8)
+            if not avoine:
+                pyautogui.mouseUp()
+                if map1:
+                    print("Map 1")
+                    pyautogui.moveTo(90, 720)
+                    pyautogui.click()
+                    time.sleep(4)
+                    print("Changement de carte")
+                elif map2:
+                    print("Map 2")
+                    pyautogui.moveTo(90, 720)
+                    pyautogui.click()
+                    time.sleep(4)
+                    print("Changement de carte")
+                elif map3:
+                    print("Map 3")
+                    pyautogui.moveTo(2045, 720)
+                    pyautogui.click()
+                    time.sleep(4)
+                    pyautogui.moveTo(2045, 720)
+                    pyautogui.click()
+                    time.sleep(4)
+                    print("Changement de carte")
+            pass
+    sell("Ressource_Flo/Ble_inventaire.png")
+    sell("Ressource_Flo/Orge_inventaire.png")
+    sell("Ressource_Flo/Avoine_inventaire.png")
