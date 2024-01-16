cda08860e07bbdb6194b90dd86e17ee9d397d14d
FloScythe <61111800+FloScythe@users.noreply.github.com>
2024-01-16 23:30:13 +0100

    V0.0.04

--- Finance/main.py
+++ Finance/main.py
@@ -15,11 +15,21 @@ def calcul_quantite(date, ticker):
 
     # Calculer la somme de la colonne "Quantite"
     total_quantity = filtered_data['Quantite'].sum()
-    if total_quantity != 0 :
+    
+    if total_quantity != 0:
         # Afficher la somme de la colonne "Quantite"
-        print(f"\nAu {date}, le nombre d'action de {ticker} était de : {total_quantity}")
-    else :
-        pass
+        print(f"\nLe nombre d'action de {ticker} était de : {total_quantity}")
+        valeur_action = recuperation_prix(date, ticker)
+        print(f"Une valeur à la cloture de : {valeur_action} €")
+        valeur_total = calcul_valeur(total_quantity, valeur_action)
+        print(f"Une valeur total dans le portefeuille de : {valeur_total} €")
+        print('\n######################################################################\n')
+        
+        # Retourner la valeur totale pour permettre à la fonction appelante de l'utiliser
+        return valeur_total
+    else:
+        # Si la quantité est zéro, retourner 0 (ou une autre valeur par défaut)
+        return 0
 
 
 def dernier_jour_ouvre_avant_ou_veille(date):
@@ -31,7 +41,7 @@ def dernier_jour_ouvre_avant_ou_veille(date):
 
     while veille.weekday() >= 5 or veille.strftime('%Y-%m-%d') in holidays:
         veille -= pd.Timedelta(days=1)
-
+        
     return veille
 
 
@@ -52,12 +62,34 @@ def recuperation_prix(date, ticker):
 
     if not data.empty:
         # Récupérer la valeur de clôture de l'action
-        last_value = round(data['Close'][0],2)
-        print(f"\nLa valeur de clôture de l'action {ticker} à la date {date_str_end} est de : {last_value}")
+        last_value = round(data['Close'][0],3)
+        return last_value
     else:
         print(f"\nAucune donnée disponible pour {ticker} à la date {date_str_end}.")
+        return 0
 
 
+def calcul_valeur(quantite, prix):
+    valeur = round(quantite * prix,3)
+    return valeur
+
+
+# Définir les dates
+date_debut_str = "2022-01-10"
+date_fin = datetime.now().date()
+date_fin_str = date_fin.strftime('%Y-%m-%d')
+
+# Convertir les chaînes de caractères en objets datetime
+date_debut = datetime.strptime(date_debut_str, "%Y-%m-%d")
+date_fin = datetime.strptime(date_fin_str, "%Y-%m-%d")
+
+# Calculer la différence entre les deux dates
+difference = date_fin - date_debut
+
+# Extraire le nombre de jours à partir de la différence
+nombre_de_jours = difference.days
+# Afficher le résultat
+print(f"Il y a {nombre_de_jours} jours entre {date_debut_str} et {date_fin_str}.")
 
 # Liste des jours fériés
 holidays = ['2022-01-01', '2022-12-25']
@@ -65,10 +97,33 @@ holidays = ['2022-01-01', '2022-12-25']
 #Liste des actions détenue
 action = ['PE500.PA','PANX.PA','LQQ.PA','EWLD.PA']
 
-date = '2022-01-15'
-# Afficher la valeur de clôture de l'action
-for i in range(len(action)):
-    calcul_quantite(date, action[i])
-    recuperation_prix(date, action[i])
-    print('\n######################################################################\n')
+# Date de recherche
+date = '2023-06-25'
+
+#-------------------------------Affichage et recherche du prix de l'action à un jour J--------------------------------------------
+# Nouvelle liste pour stocker les sommes quotidiennes
+sommes_quotidiennes = []
+
+# Afficher la valeur de clôture des actions pour chaque jour entre date_debut_str et date_fin_str
+print(f"\nLe portefeuille entre {date_debut_str} et {date_fin_str} était composé de :")
+
+# Boucle à travers chaque jour
+for i in range(nombre_de_jours + 1):  # +1 pour inclure également la date de début
+    date_courante = (date_debut + timedelta(days=i)).strftime('%Y-%m-%d')
+    
+    somme_valeur_total_jour = 0  # Initialiser la somme pour la journée
+    
+    for action_ticker in action:
+        valeur_total = calcul_quantite(date_courante, action_ticker)
+        somme_valeur_total_jour += valeur_total
+
+    # Ajouter la somme de la journée à la liste
+    sommes_quotidiennes.append(somme_valeur_total_jour)
+
+# Afficher les sommes quotidiennes
+for i, somme_jour in enumerate(sommes_quotidiennes, start=1):
+    print(f"Somme des 'valeur_total' pour le jour {i}: {somme_jour} €")
+
+
+#--------------------------------------------------------------------------------------------------------------------------------
 
