9d9f3244e04dfad41a2de258283003f5ca72db0a
FloScythe <61111800+FloScythe@users.noreply.github.com>
2024-01-16 22:41:02 +0100

    V0.0.03

--- Finance/main.py
+++ Finance/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import numpy as np
+from datetime import datetime, timedelta
 
 def calcul_quantite(date, ticker):
     # Lire le fichier CSV
@@ -14,45 +15,60 @@ def calcul_quantite(date, ticker):
 
     # Calculer la somme de la colonne "Quantite"
     total_quantity = filtered_data['Quantite'].sum()
+    if total_quantity != 0 :
+        # Afficher la somme de la colonne "Quantite"
+        print(f"\nAu {date}, le nombre d'action de {ticker} était de : {total_quantity}")
+    else :
+        pass
 
-    # Afficher la somme de la colonne "Quantite"
-    print(f"\nAu {date}, le nombre d'action de {ticker} était de : {total_quantity}")
 
-
-# Liste des jours fériés
-holidays = ['2022-01-01', '2022-12-25']
-
-def dernier_jour_ouvre_avant(date):
+def dernier_jour_ouvre_avant_ou_veille(date):
+    # Convertir la date en datetime
     date = pd.to_datetime(date)
-    while date.weekday() > 5 or date.strftime('%Y-%m-%d') in holidays:
-        date -= pd.Timedelta(days=1)
-    return date
+
+    # Trouver la date de la veille
+    veille = date - timedelta(days=1)
+
+    while veille.weekday() >= 5 or veille.strftime('%Y-%m-%d') in holidays:
+        veille -= pd.Timedelta(days=1)
+
+    return veille
 
 
 def recuperation_prix(date, ticker):
     # Convertir la date en datetime
     date = pd.to_datetime(date)
 
-    # Trouver le dernier jour ouvré avant la date
-    last_business_day = dernier_jour_ouvre_avant(date)
+    # Trouver le dernier jour ouvré avant ou incluant la date
+    last_business_day = dernier_jour_ouvre_avant_ou_veille(date)
 
     # Convertir la date en chaîne de caractères
-    date_str = last_business_day.strftime('%Y-%m-%d')
+    date_str_start = last_business_day.strftime('%Y-%m-%d')
+    date_str_end = date.strftime('%Y-%m-%d')
+
 
     # Récupérer les données boursières d'une action précise
-    data = yf.download(ticker, start=date_str, end=date_str)
+    data = yf.download(ticker, start=date_str_start, end=date_str_end)
 
     if not data.empty:
         # Récupérer la valeur de clôture de l'action
-        last_value = data['Close'][0]
-        print(f"\nLa valeur de clôture de l'action {ticker} à la date {date_str} est de : {last_value}")
+        last_value = round(data['Close'][0],2)
+        print(f"\nLa valeur de clôture de l'action {ticker} à la date {date_str_end} est de : {last_value}")
     else:
-        print(f"\nAucune donnée disponible pour {ticker} à la date {date_str}.")
+        print(f"\nAucune donnée disponible pour {ticker} à la date {date_str_end}.")
 
 
+
+# Liste des jours fériés
+holidays = ['2022-01-01', '2022-12-25']
+
+#Liste des actions détenue
 action = ['PE500.PA','PANX.PA','LQQ.PA','EWLD.PA']
 
-calcul_quantite('2022-01-20', action[0])
-print('\n######################################################################\n')
+date = '2022-01-15'
 # Afficher la valeur de clôture de l'action
-recuperation_prix('2020-01-20', action[0])
+for i in range(len(action)):
+    calcul_quantite(date, action[i])
+    recuperation_prix(date, action[i])
+    print('\n######################################################################\n')
+
