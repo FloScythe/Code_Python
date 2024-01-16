2f52b37caac209d48808fb7f394fbd9d1887ba06
FloScythe <61111800+FloScythe@users.noreply.github.com>
2024-01-16 21:58:09 +0100

    V0.0.02

--- Finance/main.py
+++ Finance/main.py
@@ -1,35 +1,58 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import yfinance as yf
+import numpy as np
 
-# Lire le fichier CSV
-data = pd.read_csv('Data/Data.csv', delimiter=';')
+def calcul_quantite(date, ticker):
+    # Lire le fichier CSV
+    data = pd.read_csv('Data/Data.csv', delimiter=';')
 
-# Compter le nombre d'occurrences de chaque nom de la colonne "Tickers"
-counts = data['Tickers'].value_counts()
+    # Convertir la colonne "Date" en datetime
+    data['Date'] = pd.to_datetime(data['Date'], format='%d/%m/%Y')
 
-# Retirer une valeur spécifique
-counts = counts.drop('Cash')
+    # Filtrer les données par date et par ticker
+    filtered_data = data[(data['Date'] < date) & (data['Tickers'] == ticker)]
 
-# Afficher un graphique en barres
-counts.plot(kind='bar')
+    # Calculer la somme de la colonne "Quantite"
+    total_quantity = filtered_data['Quantite'].sum()
 
-"""
-Différent type de grapphique :
-    
-Graphique en barres : df.plot(kind='bar')
-Graphique en barres empilées : df.plot(kind='bar', stacked=True)
-Graphique en histogramme : df.plot(kind='hist')
-Graphique en boîte : df.plot(kind='box')
-Graphique en nuage de points : df.plot(kind='scatter', x='x_column', y='y_column')
-Graphique en secteurs : df.plot(kind='pie')
-Graphique en surface : df.plot(kind='surface')
-Graphique en densité : df.plot(kind='density')
-"""
+    # Afficher la somme de la colonne "Quantite"
+    print(f"\nAu {date}, le nombre d'action de {ticker} était de : {total_quantity}")
 
-# Ajouter un titre et des étiquettes d'axe
-plt.title('Occurrences de chaque nom de la colonne "Tickers"')
-plt.xlabel('Nom de la colonne "Tickers"')
-plt.ylabel('Nombre d\'occurrences')
 
-# Afficher le graphique
-plt.show()
+# Liste des jours fériés
+holidays = ['2022-01-01', '2022-12-25']
+
+def dernier_jour_ouvre_avant(date):
+    date = pd.to_datetime(date)
+    while date.weekday() > 5 or date.strftime('%Y-%m-%d') in holidays:
+        date -= pd.Timedelta(days=1)
+    return date
+
+
+def recuperation_prix(date, ticker):
+    # Convertir la date en datetime
+    date = pd.to_datetime(date)
+
+    # Trouver le dernier jour ouvré avant la date
+    last_business_day = dernier_jour_ouvre_avant(date)
+
+    # Convertir la date en chaîne de caractères
+    date_str = last_business_day.strftime('%Y-%m-%d')
+
+    # Récupérer les données boursières d'une action précise
+    data = yf.download(ticker, start=date_str, end=date_str)
+
+    if not data.empty:
+        # Récupérer la valeur de clôture de l'action
+        last_value = data['Close'][0]
+        print(f"\nLa valeur de clôture de l'action {ticker} à la date {date_str} est de : {last_value}")
+    else:
+        print(f"\nAucune donnée disponible pour {ticker} à la date {date_str}.")
+
+
+action = ['PE500.PA','PANX.PA','LQQ.PA','EWLD.PA']
+
+calcul_quantite('2022-01-20', action[0])
+print('\n######################################################################\n')
+# Afficher la valeur de clôture de l'action
+recuperation_prix('2020-01-20', action[0])
