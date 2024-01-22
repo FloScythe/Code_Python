8d0e4c03c5c0aa50e70c0eae56aff8e6da752eeb
FloScythe <61111800+FloScythe@users.noreply.github.com>
2024-01-22 20:02:45 +0100

    V0.0.08

--- Finance/Test.py
+++ Finance/Test.py
@@ -3,106 +3,26 @@ import yfinance as yf
 from datetime import datetime, timedelta
 import logging
 
-# Fonction pour télécharger les données boursières
-def download_stock_data(tickers, start_date, end_date):
-    try:
-        data = yf.download(tickers, start=start_date, end=end_date)['Close']
-        return data
-    except Exception as e:
-        logging.error(f"Erreur lors de la récupération des données : {e}")
-        return pd.DataFrame()
-
-# Fonction pour calculer la valeur du portefeuille
-def calculate_portfolio_value(file_path, date):
-    df = pd.read_csv(file_path, delimiter=';')
-
-    df['Date'] = pd.to_datetime(df['Date'])
-
-    df_filter = df[df['Date'] <= date]
-    tickers = df_filter['Tickers'].unique().tolist()
-
-    data = download_stock_data(tickers, df_filter['Date'].min(), date)['Close']
-
-    buy_df = df_filter[df_filter['Action'] == 'Achat']
-    buy_amounts_per_stock = buy_df.groupby('Tickers')['Montant'].sum()
-#--------------------------------------------
-    deposit_sum = df_filter[df_filter['Action'] == 'Depot']['Montant'].sum()
-    buy_sum = buy_amounts_per_stock.sum()
-    sell_sum = df_filter[df_filter['Action'] == 'Vente']['Montant'].sum()
-    cash_sum = deposit_sum - buy_sum - sell_sum
-
-    stock_product = data.iloc[-1].mul(df_filter.groupby('Tickers')['Quantite'].sum(), fill_value=0)
-    portfolio_value = stock_product.sum() + cash_sum
-    
-    return portfolio_value,buy_amounts_per_stock
-
 # Configurer le module de journalisation
 logging.basicConfig(filename='logfile.log', level=logging.ERROR)
 
 file_path = 'Data/Data.csv'
-start_date = datetime(2024, 1, 15)
-today_date = datetime.today()
+date_specifiee = datetime(2022, 1, 31)
 
 df = pd.read_csv(file_path, delimiter=';')
 
 df['Date'] = pd.to_datetime(df['Date'])
 df = df.sort_values(by='Date')
 
-df_filter = df[df['Date'] <= start_date]
+df_filtre = df[df['Date'] <= date_specifiee]
 
 # Utilisation de yf.download pour récupérer les données en une seule requête
-tickers = df_filter['Tickers'].unique().tolist()
+tickers = df_filtre['Tickers'].unique().tolist()
 
 try:
-    data = download_stock_data(tickers, df_filter['Date'].min(), start_date)
+    data = yf.download(tickers, start=df_filtre['Date'].min(), end=date_specifiee)['Close']
 except Exception as e:
-    logging.error(f"Erreur lors de la récupération des données : {e}")
+    logging.error(f"Erreur lors de la récupération des données : {str(e)}")
     data = pd.DataFrame()
 
-# Calculer la somme des espèces restantes
-deposit_sum = df_filter[df_filter['Action'] == 'Depot']['Montant'].sum()
-# Appeler la fonction pour calculer la valeur du portefeuille et obtenir buy_amounts_per_stock
-portfolio_value, buy_amounts_per_stock = calculate_portfolio_value(file_path, start_date)
-
-buy_sum = buy_amounts_per_stock.sum()
-sell_sum = df_filter[df_filter['Action'] == 'Vente']['Montant'].sum()
-cash_sum = deposit_sum - buy_sum - sell_sum
-
-# Initialiser le DataFrame pour stocker les rendements
-returns_df = pd.DataFrame(columns=['Date', 'Valeur du portefeuille', 'Total Depose', 'Difference', 'HP'])
-
-# Boucle pour chaque jour entre la date de début et la date de fin
-current_date = start_date
-
-while current_date <= today_date:
-    # Filtrer les transactions jusqu'à la date actuelle
-    transactions_day = df_filter[df_filter['Date'] == current_date]
-
-    # Calculer la somme des dépôts jusqu'à la date actuelle
-    total_deposit = df_filter[df_filter['Date'] <= current_date][df_filter['Action'] == 'Depot']['Montant'].sum()
-    
-    # Récupérer la valeur du portefeuille pour la date actuelle
-    portfolio_value = calculate_portfolio_value(file_path, current_date)
-        
-    if not returns_df.empty:
-        difference = total_deposit - returns_df['Total Depose'].shift(1).loc[returns_df['Date'] == current_date - timedelta(days=1)].values[0]
-    else:
-        difference = 0    
-      
-    # Calculer le HP (Holding Period Return) avec la formule spécifiée
-    hp = 1 + (portfolio_value - (returns_df['Valeur du portefeuille'].shift(1) + difference)) / (returns_df['Valeur du portefeuille'].shift(1) + difference)
-
-    # Ajouter les résultats au DataFrame
-    returns_df = pd.concat([returns_df, pd.DataFrame({
-        'Date': [current_date],
-        'Valeur du portefeuille': [portfolio_value],
-        'Total Depose': [total_deposit],
-        'Difference': [difference],
-        'HP': [hp]
-    })], ignore_index=True)
-    
-    # Passer à la date suivante
-    current_date += timedelta(days=1)
-
-# Sauvegarder le DataFrame dans un fichier Excel
-returns_df.to_excel('Rendement.xlsx', index=False)
+print(data)
--- Finance/main.py
+++ Finance/main.py
@@ -2,57 +2,82 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime
 import logging
+import os
 
 # Configurer le module de journalisation
 logging.basicConfig(filename='logfile.log', level=logging.ERROR)
 
 file_path = 'Data/Data.csv'
-date_specifiee = datetime(2024, 1, 21)
+date_specifiee = datetime.today()
 
 df = pd.read_csv(file_path, delimiter=';')
 
 df['Date'] = pd.to_datetime(df['Date'])
 df = df.sort_values(by='Date')
 
-df_filtre = df[df['Date'] <= date_specifiee]
-
+#--------------------------------------------------------------------------------
 # Utilisation de yf.download pour récupérer les données en une seule requête
-tickers = df_filtre['Tickers'].unique().tolist()
-
+tickers = df['Tickers'].unique().tolist()
+tickers.remove('Cash')
+print(tickers)
 try:
-    data = yf.download(tickers, start=df_filtre['Date'].min(), end=date_specifiee)['Close']
+    data = yf.download(tickers, start=df['Date'].min(), end=date_specifiee)['Close']
 except Exception as e:
     logging.error(f"Erreur lors de la récupération des données : {str(e)}")
     data = pd.DataFrame()
 
-# Filtrer uniquement les lignes d'achat pour le calcul du PRU
-achats_df = df_filtre[df_filtre['Action'] == 'Achat']
+#--------------------------------------------------------------------------------
+# Créer une liste pour stocker les résultats de chaque jour
+resultats_liste = []
 
-# Calculer la somme des montants par action pour les achats
-montants_achats_par_action = achats_df.groupby('Tickers')['Montant'].sum()
+# Récupérer la première date présente dans le fichier "Data.CSV"
+premiere_date = df['Date'].min()
 
-# Calculer la somme des quantités par action pour les achats
-quantites_achats_par_action = achats_df.groupby('Tickers')['Quantite'].sum()
+# Boucle à travers les jours pour calculer les valeurs
+for date in pd.date_range(start=premiere_date, end=date_specifiee, freq='D'):
+    # Filtrer le DataFrame pour la date actuelle
+    df_date = df[df['Date'] <= date]
 
-# Calculer le PRU en excluant les transactions de vente
-pru_par_action_achats = montants_achats_par_action / quantites_achats_par_action
-# Utiliser reset_index pour afficher les tickers et les valeurs de PRU sans l'index "Tickers"
-pru_par_action_achats_reset = pru_par_action_achats.reset_index()
-# Renommer les colonnes du DataFrame résultant
-pru_par_action_achats_reset.columns = ['Tickers', 'PRU']
+    # Filtrer uniquement les lignes d'achat
+    achats_df = df_date[df_date['Action'] == 'Achat']
 
-# Calculer la somme des espèces restantes
-somme_depot = df_filtre[df_filtre['Action'] == 'Depot']['Montant'].sum()
-somme_achat = montants_achats_par_action.sum()
-somme_vente = df_filtre[df_filtre['Action'] == 'Vente']['Montant'].sum()
-somme_especes = somme_depot - somme_achat - somme_vente
+    # Calculer la somme des espèces restantes
+    somme_depot = df_date[df_date['Action'] == 'Depot']['Montant'].sum()
+    somme_achat = df_date[df_date['Action'] == 'Achat']['Montant'].sum()
+    somme_vente = df_date[df_date['Action'] == 'Vente']['Montant'].sum()
+    somme_especes = somme_depot - somme_achat - somme_vente
+    
+    # Calculer les valeurs pour la date actuelle (similaire à votre code existant)
+    valeur_portefeuille = 0  # Initialiser à une valeur par défaut
+    
+    if not data.empty:
+        # Obtenez la valeur de l'action pour la date spécifiée
+        for ticker in tickers:
+            try:
+                valeur_actions_date = data.loc[date, ticker]
+                # Calculer le produit du nombre d'actions par la valeur de l'action à la date spécifiée
+                print(date)
+                print(f"{ticker} : {achats_df[achats_df['Tickers'] == ticker]['Quantite'].sum()} à la valeur : {valeur_actions_date}")
 
-# Calculer la valeur totale du portefeuille
-if not data.empty:
-    produit_par_action = data.iloc[-1].mul(df_filtre.groupby('Tickers')['Quantite'].sum(), fill_value=0)
-    valeur_portefeuille = produit_par_action.sum() + somme_especes
-    print(f"Il reste : {somme_especes} euros en espèces")
-    print(f"La valeur totale du portefeuille à la date {date_specifiee} est de {valeur_portefeuille} euros.")
-    print(pru_par_action_achats_reset.to_string(header=True, index=False))
-else:
-    print("Erreur dans la récupération des données. Consultez le fichier journal.")
+                produit_par_action = achats_df[achats_df['Tickers'] == ticker]['Quantite'].sum() * valeur_actions_date
+                valeur_portefeuille += produit_par_action
+                print(f"La valeur du portefeuille : {valeur_portefeuille}")
+            except KeyError:
+                # Gérer l'absence de la date dans l'index de data
+                pass
+    
+    # Ajouter les valeurs à la liste des résultats
+    resultats_liste.append({
+        'Date': date,
+        'Valeur du portefeuille': valeur_portefeuille + somme_especes,
+        'Somme des dépôts': somme_depot
+    })
+
+# Créer un DataFrame à partir de la liste des résultats
+resultats_df = pd.DataFrame(resultats_liste)
+
+# Enregistrer le DataFrame dans un fichier Excel
+fichier_excel = 'resultats_portefeuille.xlsx'
+resultats_df.to_excel(fichier_excel, index=False)
+
+print(f"Données enregistrées dans {fichier_excel}")
